0db9451c48db6947fcc21f7ac25519291d2724a3
Paulo Henrique Garcia <paulohenriquesg@gmail.com>
2024-11-16 19:41:58 +0100

    Improvements on API Client / Add logger / Add default json header / handle token

--- app/src/main/java/com/paulohenriquesg/fahrenheit/api/ApiClient.kt
+++ app/src/main/java/com/paulohenriquesg/fahrenheit/api/ApiClient.kt
@@ -1,13 +1,37 @@
 // ApiClient.kt
 package com.paulohenriquesg.fahrenheit.api
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    fun create(baseUrl: String): ApiService {
+    fun create(baseUrl: String, token: String? = null): ApiService {
+        val clientBuilder = OkHttpClient.Builder()
+
+        // Add logging interceptor
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        clientBuilder.addInterceptor(logging)
+
+        clientBuilder.addInterceptor { chain ->
+            val requestBuilder: Request.Builder = chain.request().newBuilder()
+                .addHeader("Content-Type", "application/json")
+
+            if (token != null) {
+                requestBuilder.addHeader("Authorization", "Bearer $token")
+            }
+
+            chain.proceed(requestBuilder.build())
+        }
+
+        val client = clientBuilder.build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         return retrofit.create(ApiService::class.java)
