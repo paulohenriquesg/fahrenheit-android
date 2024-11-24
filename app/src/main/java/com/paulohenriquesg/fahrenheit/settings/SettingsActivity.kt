157d794baac115d693ff4c871fd3e8a594e99200
Paulo Henrique Garcia <paulohenriquesg@gmail.com>
2024-11-24 09:06:22 +0100

    Dark theme improvements on settings screen

--- app/src/main/java/com/paulohenriquesg/fahrenheit/settings/SettingsActivity.kt
+++ app/src/main/java/com/paulohenriquesg/fahrenheit/settings/SettingsActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.MaterialTheme
 import com.paulohenriquesg.fahrenheit.storage.SharedPreferencesHandler
 import com.paulohenriquesg.fahrenheit.ui.theme.FahrenheitTheme
 
@@ -68,12 +68,12 @@ fun SettingsScreen() {
             .background(MaterialTheme.colorScheme.background) // Apply background color from theme
             .padding(16.dp)
     ) {
-        Text(text = "Settings", style = MaterialTheme.typography.titleLarge)
+        Text(text = "Settings", style = MaterialTheme.typography.titleLarge,color = MaterialTheme.colorScheme.onSurface)
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "Dark Theme")
+            Text(text = "Dark Theme",color = MaterialTheme.colorScheme.onSurface)
             Spacer(modifier = Modifier.width(8.dp))
             Switch(
                 checked = isDarkTheme,
